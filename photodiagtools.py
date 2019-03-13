304de1fc0e2301c7889f515c4d83cdb4bf919cb4
Andi <andibarg@gmail.com>
2019-03-13 17:41:30 +0100

    Fix error if no EXIF data found

--- photodiagtools.py
+++ photodiagtools.py
@@ -20,11 +20,14 @@ class picture:
         self.img = np.array(pic)
 
         # Extract Exif data
-        self.exif = {
-            PIL.ExifTags.TAGS[k]: v
-            for k, v in pic._getexif().items()
-            if k in PIL.ExifTags.TAGS
-        }
+        try:
+            self.exif = {
+                PIL.ExifTags.TAGS[k]: v
+                for k, v in pic._getexif().items()
+                if k in PIL.ExifTags.TAGS
+            }
+        except:
+            print('No EXIF data found.')
 
     '''
     RGB color histogram
@@ -53,7 +56,7 @@ class picture:
     depending on the image size.
     Outputs mask with the same size as the image.
     '''
-    def focus_peak(self,radius=1,amount=500,threshold=4000):
+    def focus_peak(self,radius=1,amount=500,threshold=6000):
         # Check if RGB or gray
         if len(self.img.shape)==3:
             im = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
@@ -130,17 +133,17 @@ if __name__ == "__main__":
     ax[0,0].imshow(pic.img)
     ax[0,0].imshow(peak,cmap=plt.cm.bwr_r)
     ax[0,0].axis("off")
-    ax[0,0].set_title("Focus peaking")
+    ax[0,0].set_title("Focus peaking",fontsize=12)
 
     ax[0,1].imshow(pic.img)
     ax[0,1].imshow(clip,cmap=plt.cm.bwr)
     ax[0,1].axis("off")
-    ax[0,1].set_title("Highlight clipping")
+    ax[0,1].set_title("Highlight clipping",fontsize=12)
 
     ax[1,0].imshow(pic.img)
-    ax[1,0].plot(xgrid, ygrid,color='k',linewidth=1)
+    ax[1,0].plot(xgrid, ygrid,color=(0,1,0),linewidth=1)
     ax[1,0].axis("off")
-    ax[1,0].set_title("Grid lines")
+    ax[1,0].set_title("Grid lines",fontsize=12)
 
     ax[1,1].bar(bins,hists[0],width=1.0,color=[1,0,0])
     ax[1,1].bar(bins,hists[1],width=1.0,color=(0,1,0))
@@ -151,7 +154,9 @@ if __name__ == "__main__":
     ax[1,1].bar(bins,np.minimum.reduce(hists),width=1.0,color=(1, 1, 1))
     ax[1,1].axis("off")
     ax[1,1].set_ylim(0,255)
-    ax[1,1].set_ylim(0,np.mean(np.minimum.reduce(hists))*10)
-    ax[1,1].set_title("RGB histogram")
+    ax[1,1].set_ylim(0,np.mean(np.minimum.reduce(hists))*4)
+    ax[1,1].set_title("RGB histogram",fontsize=12)
+
+    plt.savefig('example_diag.jpg', format='jpg', dpi=300)
     
     plt.show()
